a6eef76375ea62240f686847a46bc5622d7d3909
Thayna Peres <tpp.teodoro@gmail.com>
2024-09-14 18:17:35 -0300

    atts

---
+++ src/app/componentes/separador/separador.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-separador',
+  templateUrl: './separador.component.html',
+  styleUrl: './separador.component.css'
+})
+export class SeparadorComponent {
+
+}
