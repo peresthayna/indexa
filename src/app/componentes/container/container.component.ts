d46363b22bbabd8f19894c07a52955ac711351bb
Thayna Peres <tpp.teodoro@gmail.com>
2024-09-14 18:17:19 -0300

    atts

---
+++ src/app/componentes/container/container.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-container',
+  templateUrl: './container.component.html',
+  styleUrl: './container.component.css'
+})
+export class ContainerComponent {
+	public alfabeto: string[] = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+}
