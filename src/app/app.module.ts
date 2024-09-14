a17e174e6e844d788d02db0278d636a9144a904b
Thayna Peres <tpp.teodoro@gmail.com>
2024-09-14 18:17:09 -0300

    atts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContainerComponent } from './componentes/container/container.component';
+import { HomeComponent } from './main/home/home.component';
+import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
+import { SeparadorComponent } from './componentes/separador/separador.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContainerComponent,
+    HomeComponent,
+    CabecalhoComponent,
+    SeparadorComponent
   ],
   imports: [
     BrowserModule,
