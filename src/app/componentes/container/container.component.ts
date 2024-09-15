20f2bee7fabc06793b0472a1dfbcdcda9db4518a
Thayna Peres <tpp.teodoro@gmail.com>
2024-09-14 21:10:25 -0300

    atts

--- src/app/componentes/container/container.component.ts
+++ src/app/componentes/container/container.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Contato } from '../../main/shared/model/contato.model';
 
 @Component({
   selector: 'app-container',
   templateUrl: './container.component.html',
   styleUrl: './container.component.css'
 })
-export class ContainerComponent {
+export class ContainerComponent implements OnInit {
 	public alfabeto: string[] = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  	public contato: Contato = new Contato();
+  	public busca: string = '';
+
+    ngOnInit(): void {
+        this.contato.nome = 'Thayná';
+        this.contato.telefone = '+55 18 998765432';
+    }
+
+    //filtrar contatos por letra inicial
+    //filtrar contatos por busca (remover acentos primeiro e colocar em minúsculas)
 }
